4f59e7b1b73d0ac3a1204a8512a9c3171309757c
pmcarpan <arpan0123@gmail.com>
2020-12-05 20:59:17 +0530

    fix the product ids to be in serial order instead of all same

--- src/main/java/maven/demo/repository/ProductRepository.java
+++ src/main/java/maven/demo/repository/ProductRepository.java
@@ -7,9 +7,9 @@ import maven.demo.model.Product;
 public class ProductRepository {
     private static List<Product> products = List.of(
         Product.of(1L, "Lenovo Legion",     60000.90),
-        Product.of(1L, "Raspberry Pi 4",    3950.00),
-        Product.of(1L, "USB Splitter",      599.00),
-        Product.of(1L, "Philips Toaster",   1800.50)
+        Product.of(2L, "Raspberry Pi 4",    3950.00),
+        Product.of(3L, "USB Splitter",      599.00),
+        Product.of(4L, "Philips Toaster",   1800.50)
     );
 
     public static List<Product> getProducts() {
